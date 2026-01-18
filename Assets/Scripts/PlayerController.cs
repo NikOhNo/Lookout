1cbb3e2083a6ef7dbce953cf01059739e8605374
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2026-01-17 21:30:12 -0800

    2.5D shader and movement!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,44 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 25f;
     [SerializeField] float groundDist;
     [SerializeField] LayerMask terrainLayer;
-    [SerializeField] Rigidbody rb;
-    [SerializeField] SpriteRenderer sr;
+    [SerializeField] CharacterController controller;
+    [SerializeField] GameObject spriteObject;
+
+    Vector2 moveInput;
+    Animator Animator => GetComponent<Animator>();
+
+    private void Update()
+    {
+        Vector3 move = moveSpeed * Time.deltaTime * new Vector3(moveInput.x, 0, moveInput.y);
+
+        if (controller.isGrounded)
+        {
+            move.y = -1f;
+        }
+        else
+        {
+             move.y += Physics.gravity.y * Time.deltaTime;
+        }
+        controller.Move(move);
+    }
 
     public void HandleMove(InputAction.CallbackContext context)
     {
-        Vector2 moveVector = context.ReadValue<Vector2>();
-        rb.linearVelocity = new Vector3(moveVector.x, 0, moveVector.y) * moveSpeed;
+        moveInput = context.ReadValue<Vector2>();
+        Animator.SetFloat("x", moveInput.x);
+        HandleSpriteFlip();
+        Animator.SetFloat("z", moveInput.y);
+    }
+
+    private void HandleSpriteFlip()
+    {
+        if (moveInput.x < 0)
+        {
+            spriteObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            spriteObject.transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
